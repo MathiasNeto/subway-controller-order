e8d412bf947e6e4e9bd11b985f0090756afc50a0
Emanuel Matias <matias.neto.edu@gmail.com>
2023-10-17 15:57:57 -0300

    bread builder

---
+++ src/main/java/org/mixmatias/interfaces/Builder.java
@@ -0,0 +1,11 @@
+package org.mixmatias.interfaces;
+
+import java.util.List;
+
+public interface Builder<k, S>{
+    k filling(String r);
+    k sauce(List<String> m);
+    k more (String m);
+
+    S createdBread();
+}
